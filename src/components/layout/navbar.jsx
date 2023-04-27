c31014388f5f9a17949ef6fcf3441dd090399d1c
PranTanTheMan <Seriousbro23@gmail.com>
2023-04-26 23:05:02 -0700

    Next UI init

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,5 +1,29 @@
-import React from "react";
-import Header from "../header";
-export default function Navbar() {
-  return <Header />;
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+
+export default function Header() {
+  return (
+    <Navbar isBordered variant="floating">
+      <Navbar.Brand>
+        <Text b color="inherit" hideIn="xs">
+          Tech Optimum
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content hideIn="xs" variant="highlight-rounded">
+        <Navbar.Link href="#">Features</Navbar.Link>
+        <Navbar.Link href="#">Customers</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="inherit" href="#">
+          Login
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Sign Up
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
-
-import { Heading } from "@chakra-ui/react";
-import Layout from "@/components/layout/layout";
-import Temp from "@/components/temp";
-
-import Landing from "@/components/landing";
+import { Button } from "@nextui-org/react";
+import Header from "../components/layout/navbar";
 
 export default function Home() {
   return (
@@ -19,9 +15,8 @@ export default function Home() {
       </Head>
 
       {/* CONTENT */}
-      <Layout>
-        <Landing />
-      </Layout>
+
+      <Header />
     </>
   );
 }
